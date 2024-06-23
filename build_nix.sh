59cd4d38577b4bf0e37ce29edca269f164c921c2
t3mp0r4ry <80374341+t3mp0r4ry23@users.noreply.github.com>
2024-06-23 16:54:15 -0700

    restructure to be more in line with traditional devkitpro based projects

--- build_nix.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cd src && make clean && make -j4 && cp ./sdl2-doom ../sdl2-doom
--- build_windows.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cd src && make clean && make -j4 -f makefile.mingw && cp ./sdl2-doom.exe ../sdl2-doom.exe
