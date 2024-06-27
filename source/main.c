f5e7fd51f1ae857b69df864bdc542cbe4d330d58
t3mp0r4ry <80374341+t3mp0r4ry23@users.noreply.github.com>
2024-06-26 21:36:54 -0700

    attempt to add pwad loading functionality (wip)

--- source/main.c
+++ source/main.c
@@ -13,16 +13,17 @@ static void *xfb = NULL;
 static GXRModeObj *rmode = NULL;
 
 int iwadCount = 0;
-int pwadCount = 0;
-int selectedPWADCount = 0;
+int pwadsCount = 0;
+int selectedPWADsCount = 0;
 int done = 0;
 int cursorLine = 0;
+int customargc = 0;
 char **customargv;
 
 int fileCount(char *path) {
 	int count = 0;
 	DIR *count_dir;
-	struct dirent *count_ent
+	struct dirent *count_ent;
 
 	count_dir = opendir(path);
 	if (count_dir != NULL) {
@@ -31,6 +32,7 @@ int fileCount(char *path) {
 				count++;
 			}
 		}
+		closedir(count_dir);
 	} else {
 		return 0;
 	}
@@ -42,16 +44,17 @@ void getFileList(char *path, char **file_list) {
 	DIR *list_dir;
 	struct dirent *list_item;
 	int list_entry = 0;
+	file_list = malloc(sizeof(char*) * fileCount(path));
 
 	list_dir = opendir(path);
 	if (list_dir != NULL) {
-		file_list = malloc(sizeof(char*) * fileCount(list_dir));
 		while (list_item = readdir(list_dir)) {
 			if (list_item->d_type == DT_REG) {
 				file_list[list_entry] = malloc(strlen(list_item->d_name) + 1);
 				strcpy(file_list[list_entry], list_item->d_name);
 			}
 		}
+		closedir(list_dir);
 	}
 }
 
@@ -90,7 +93,7 @@ int sdInit() {
 
 int main(int argc, char **argv) {
 	menuInit();
-	if (!sdInit()) {
+	if (sdInit()) {
 		printf("\nERROR:\nCould not mount SD Card.\n\nPress A to exit.\n");
 		while (1) {
 			PAD_ScanPads();
@@ -100,10 +103,14 @@ int main(int argc, char **argv) {
 	}
 
 	char **iwads;
+	getFileList("sd:/sdl2-doom/iwads", iwads);
 	iwadCount = fileCount("sd:/sdl2-doom/iwads");
+	int selectedIWAD = 0;
 	char **pwads;
-	pwadCount = fileCount("sd:/sdl2-doom/pwads");
-	int selectedPWADs[pwadCount];
+	getFileList("sd:/sdl2-doom/pwads", pwads);
+	pwadsCount = fileCount("sd:/sdl2-doom/pwads");
+	int *selectedPWADs;
+	selectedPWADs = malloc(sizeof(int) * pwadsCount);
 
 	if (!iwadCount) {
 		printf("\nERROR:\nNo IWADs found.\nPlease place one or more IWADS in a folder at [SD ROOT]/sdl2-doom/iwads/\n\nPress A to exit.\n");
@@ -116,11 +123,10 @@ int main(int argc, char **argv) {
 
 	printf("\nPress A to select an IWAD\n\n");
 	printf("\x1b[1B\x1b[999D\x1b[s");
-
 	while (!done) {
 		PAD_ScanPads();
-		for (int line = 0; line < wadCount; line++) {
-			printf("%c %s%s\x1b[40;0m\x1b[37;0m\n", line == cursorLine ? '>' : ' ', line == cursorLine ? "\x1b[47;0m\x1b[30;0m" : "", wads[line]);
+		for (int line = 0; line < iwadCount; line++) {
+			printf("%c %s%s\x1b[40;0m\x1b[37;0m\n", line == cursorLine ? '>' : ' ', line == cursorLine ? "\x1b[47;0m\x1b[30;0m" : "", iwads[line]);
 		}
 
 		if ((PAD_ButtonsDown(0) & PAD_BUTTON_DOWN) || PAD_StickY(0) > 10) {
@@ -138,23 +144,24 @@ int main(int argc, char **argv) {
 			}
 		}
 		if (PAD_ButtonsDown(0) & PAD_BUTTON_A) {
+			selectedIWAD = cursorLine;
 			done = 1;
 		}
 		VIDEO_WaitVSync();
 		printf("\x1b[u\x1b[0J");
 	}
 
-	if (pwadCount) {
+	if (pwadsCount) {
 		printf("\x1b[2J");
 		done = 0;
-		consoleLine = 0;
+		cursorLine = 0;
 
 		printf("\nPress A to select PWADs. Press START to finish selection.\n\n");
 		printf("\x1b[1B\x1b[999D\x1b[s");
 		while (!done) {
 			PAD_ScanPads();
-			for (int line = 0; line < wadCount; line++) {
-				printf("%c %s%s%s\x1b[40;0m\x1b[37;0m\n", line == cursorLine ? '>' : ' ', selectedPWADs[line] == 1 ? "\x1b[42;0m", line == cursorLine ? "\x1b[47;0m\x1b[30;0m" : "", wads[line]);
+			for (int line = 0; line < pwadsCount; line++) {
+				printf("%c %s%s%s\x1b[40;0m\x1b[37;0m\n", line == cursorLine ? '>' : ' ', selectedPWADs[line] ? "\x1b[42;0m" : "", line == cursorLine ? "\x1b[47;0m\x1b[30;0m" : "", pwads[line]);
 			}
 
 			if ((PAD_ButtonsDown(0) & PAD_BUTTON_DOWN) || PAD_StickY(0) > 10) {
@@ -175,19 +182,37 @@ int main(int argc, char **argv) {
 				done = 1;
 			}
 			if (PAD_ButtonsDown(0) & PAD_BUTTON_A) {
-				selectedPWADs[cursorline] = 1;
+				if (selectedPWADs[cursorLine]) {
+					selectedPWADs[cursorLine] = 0;
+					selectedPWADsCount--;
+				} else {
+					selectedPWADs[cursorLine] = 1;
+					selectedPWADsCount++;
+				}
 			}
 			VIDEO_WaitVSync();
 			printf("\x1b[u\x1b[0J");
 		}
 	}
 
-	customargv = malloc(sizeof(char*) * (3 + selectedPWADCount));
+	customargv = malloc(sizeof(char*) * (3 + selectedPWADsCount + (selectedPWADsCount ? 1 : 0)));
 	customargv[0] = '\0';
 	customargv[1] = malloc(sizeof(char) * 6);
 	strcpy(customargv[1], "-iwad");
-	customargv[2] = malloc(sizeof(char) * (strlen(wads[cursorLine]) + 1));
-	sprintf(customargv[2], "%s/%s", "sd:/sdl2-doom/wads/", wads[cursorLine]);
+	customargv[2] = malloc(sizeof(char) * (strlen(iwads[selectedIWAD]) + 1));
+	sprintf(customargv[2], "%s/%s", "sd:/sdl2-doom/iwads", iwads[selectedIWAD]);
+	if (selectedPWADsCount) {
+		customargc++;
+		customargv[3] = malloc(sizeof(char) * 6);
+		strcpy(customargv[3], "-file");
+		for (int pwad = 0; pwad < pwadsCount; pwad++) {
+			if (selectedPWADs[pwad]) {
+				customargv[customargc] = malloc(sizeof(char) * (strlen(pwads[pwad]) + 1));
+				sprintf(customargv[customargc], "%s/%s", "sd:/sdl2-doom/pwads", pwads[pwad]);
+				customargc++;
+			}
+		}
+	}
 
-	return I_Main(3, customargv);
+	return I_Main(customargc, customargv);
 }
